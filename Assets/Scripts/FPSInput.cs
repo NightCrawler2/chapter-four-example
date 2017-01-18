25a944f889e540bd917dec506bd42220537cd424
Sergey Beketov <sergey_2993@mail.ru>
2017-01-18 03:07:52 +0300

    initial commit

---
+++ Assets/Scripts/FPSInput.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
+public class FPSInput : MonoBehaviour {
+
+    private CharacterController _charController;
+
+    public float Speed = 6.0f;
+    public float Gravity = -9.8f;
+
+	// Use this for initialization
+    private void Start () {
+        _charController = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+	
+	// Update is called once per frame
+    private void Update () {
+        var deltaX = Input.GetAxis("Horizontal") * Speed;
+        var deltaZ = Input.GetAxis("Vertical") * Speed;
+
+        var movement = new Vector3(deltaX, 0, deltaZ);
+
+        movement = Vector3.ClampMagnitude(movement, Speed);
+        movement.y = Gravity;
+
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+
+        _charController.Move(movement);
+	}
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour {
+    public enum RotationAxes
+    {
+        MouseXnY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+    public RotationAxes Axes;
+    public float SensitivityHor = 6.0f;
+    public float SensitivityVert = 6.0f;
+    public float MaxVert = 45.0f;
+    public float MinVert = -45.0f;
+
+    private float _rotationX;
+    // Use this for initialization
+    private void Start () {
+        var body = GetComponent<Rigidbody>();
+        if (body == null) return;
+        body.freezeRotation = true;
+    }
+	
+	// Update is called once per frame
+    private void Update ()
+	{
+	    switch (Axes)
+	    {
+	        case RotationAxes.MouseX:
+	            transform.Rotate(0, Input.GetAxis("Mouse X") * SensitivityHor, 0);
+	            break;
+	        case RotationAxes.MouseY:
+	        {
+	            _rotationX -= Input.GetAxis("Mouse Y") * SensitivityVert;
+	            _rotationX = Mathf.Clamp(_rotationX, MinVert, MaxVert);
+
+	            var rotationY = transform.localEulerAngles.y;
+
+	            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+	        }
+	            break;
+	        case RotationAxes.MouseXnY:
+	        {
+	            _rotationX -= Input.GetAxis("Mouse Y") * SensitivityVert;
+	            _rotationX = Mathf.Clamp(_rotationX, MinVert, MaxVert);
+
+	            var delta = Input.GetAxis("Mouse X") * SensitivityHor;
+	            var rotationY = transform.localEulerAngles.y + delta;
+
+	            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+	        }
+	            break;
+	        default:
+	            throw new ArgumentOutOfRangeException();
+	    }
+	}
+}
